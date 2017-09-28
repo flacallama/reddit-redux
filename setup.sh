40722ecb9bbe7d4f4a48a66d0eb55f12c40f2d98
David Bayless <cellosymphonique@gmail.com>
2017-09-28 11:09:09 -0700

    Rollback changes for updating

--- setup.sh
+++ setup.sh
@@ -5,9 +5,10 @@ cd ~/workspace
 if [ -d ~/workspace/blueit_api ]; then
   cd blueit_api
   git pull origin master
+  knex migrate:rollback
 else
   git clone https://github.com/DavidBayless/blueit_api.git
-cd blueit_api
+  cd blueit_api
 fi
 npm install
 createdb blueit_api
